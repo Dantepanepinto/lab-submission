4b5d9abc9ce6a465c21760c45512269996ce8944
Dante Panepinto <panepin3@cyber.canisius.edu@UBU-SH1028-05.cyber.canisius.edu>
2023-02-08 12:54:20 -0500

    Car.Java

---
+++ Lab3/Car.java
@@ -0,0 +1,24 @@
+class Car{
+	
+	private double gallons = 0;
+	private double Milespergallon = 0;
+	
+	public void miles_driven(int miles){
+		for(int i = 0; i < miles; i ++)
+			if milespergallon % miles(
+				gallons = gallons - 1;)
+		
+	}
+	
+	public void add_gas(){
+		
+		
+	}
+	
+	public void current_distance_fuel(){
+		
+	}
+	
+	public void total_gallons_used(){
+		
+	}
