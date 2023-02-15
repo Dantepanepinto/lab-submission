11e6a7db9240d2e514cecb1dd0975a558564f50b
Dante Panepinto <panepin3@cyber.canisius.edu@UBU-SH1028-05.cyber.canisius.edu>
2023-02-15 13:10:54 -0500

    hello

--- Lab3/Car.java
+++ Lab3/Car.java
@@ -2,23 +2,38 @@ class Car{
 	
 	private double gallons = 0;
 	private double Milespergallon = 0;
+	private double distance = 0;
+	private double Total_gallons_used =  0;
 	
 	public void miles_driven(int miles){
-		for(int i = 0; i < miles; i ++)
-			if milespergallon % miles(
-				gallons = gallons - 1;)
+		distance += miles;
+		public int number = 0;
+		while(number < miles){
+			number += 1;
+			if (miles = Milespergallon){
+				number = 0;
+				Total_gallons_used += 1;
+				gallons = gallons - 1;}
 		
 	}
 	
-	public void add_gas(){
-		
+	public void add_gas(int gas){
+		gallons = gallons + gas;
 		
 	}
 	
 	public void current_distance_fuel(){
-		
+		System.out.print("Your current distance is: ");
+		System.out.print(distance);
+		System.out.print("/n");
+		System.out.print("You currently have");
+		System.out.print(gallons);
+		System.out.print(" gallons");
 	}
 	
 	public void total_gallons_used(){
+		System.out.print("Your total gas usage is: ");
+		System.out.print(Total_gallons_used);
 		
 	}
+}
