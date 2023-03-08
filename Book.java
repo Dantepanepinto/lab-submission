d8f80f06393508ed93ddea4a3706e6fe776cbd93
Dantepanepinto <panepin3@canisius.edu>
2023-03-08 12:48:15 -0500

    hag

--- Book.java
+++ Book.java
@@ -1,5 +1,4 @@
 public class Book {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+
     }
 }
--- Magazine.java
+++ Magazine.java
@@ -1,2 +1,25 @@
-public class Magazine {
+import java.time.Localdate;
+import java.util.ArrayList;
+
+public class Magazine extends Item{
+    private LocalDate date;
+    private String issueNumber;
+
+    public Magazine(String title, String location, LocalDate date, String issueNumber){
+        super(title, location, borrowable: false);
+        this.date = date;
+        this.issueNumber = issueNumber;
+    }
+
+    public Localdate getyear(){
+        return date;
+    }
+
+    public String getIssueNumber() {
+        return issueNumber;
+    }
+
+    public String prettyprint(){
+        return null;
+    }
 }
